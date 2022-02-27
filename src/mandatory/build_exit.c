34f3dda5c79999320c43d786b24f68f383dddc99
Piri Veeta <pveeta@at-e5.msk.21-school.ru>
2022-02-27 14:33:56 +0300

    files for build_in

--- src/mandatory/build_exit.c
+++ src/mandatory/build_exit.c
@@ -1,26 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   build_pwd.c                                        :+:      :+:    :+:   */
+/*   build_exit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pveeta <pveeta@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/27 14:29:10 by pveeta            #+#    #+#             */
-/*   Updated: 2022/02/27 14:29:12 by pveeta           ###   ########.fr       */
+/*   Created: 2022/02/24 22:03:32 by pveeta            #+#    #+#             */
+/*   Updated: 2022/02/24 22:03:37 by pveeta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "minishell.h"
-
-int	launch_pwd(t_input *input)
-{
-	char	*pwd;
-
-	pwd = getcwd(NULL, 0); //получаем абсолютный путь к рабочему каталогу
-	if (!pwd)
-		print_error(input, errno, "getcwd", NULL); 
-	printf("%s\n", pwd);
-	free(pwd);
-	return (0);
-}
